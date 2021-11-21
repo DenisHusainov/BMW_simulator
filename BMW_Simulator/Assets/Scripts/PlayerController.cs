5d5276a67ca1c9adb86d360e1f47fdb346c9d2b0
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2021-11-21 12:53:34 +0300

    Add canvas, VC and some one

---
+++ BMW_Simulator/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rd;
+    [SerializeField] private float _speed = default;
+    [SerializeField] private float _turnSpeed = 10f;
+    [SerializeField] private Button _rightArrow;
+    [SerializeField] private Button _leftArrow;
+
+
+    private void Awake()
+    {
+        _rd = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _rd.AddForce(Vector3.forward * _speed * Time.fixedDeltaTime);
+    }
+
+    /*public void Move()
+    {
+        if (_rightArrow)
+        {
+            transform.Translate(Vector3.right * _turnSpeed * Time.deltaTime);
+        }
+        if (_leftArrow)
+        {
+            transform.Translate(Vector3.left * _turnSpeed * Time.deltaTime);
+        }
+    }*/
+   
+}
