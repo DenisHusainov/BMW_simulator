97395191d11db123b72ea143342a4d8d12af2f94
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2021-11-21 15:23:56 +0300

    Add SpawnerRoad

--- BMW_Simulator/Assets/Scripts/PlayerController.cs
+++ BMW_Simulator/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rd;
     [SerializeField] private float _speed = default;
-    [SerializeField] private float _turnSpeed = 10f;
+    [SerializeField] private float _turnSpeed = default;
     [SerializeField] private Button _rightArrow;
     [SerializeField] private Button _leftArrow;
 
@@ -18,22 +18,27 @@ public class PlayerController : MonoBehaviour
         _rd = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rd.AddForce(Vector3.right * _turnSpeed);
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rd.AddForce(Vector3.left * _turnSpeed);
+        }
+    }
     private void FixedUpdate()
     {
         _rd.AddForce(Vector3.forward * _speed * Time.fixedDeltaTime);
+        Move();
     }
 
     public void Move()
     {
-        if (_rightArrow)
-        {
-            _rd.AddForce(Vector3.right * _turnSpeed * Time.deltaTime);
-        }
-        if (_leftArrow)
-        {
-            _rd.AddForce(Vector3.left * _turnSpeed * Time.deltaTime);
-        }
+        
     }
 
     
