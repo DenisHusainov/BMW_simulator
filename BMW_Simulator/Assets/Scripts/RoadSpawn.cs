7e58e448bb23fea083c546882754766c93d9b716
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2021-11-23 01:01:49 +0300

    Fixed map generation

--- BMW_Simulator/Assets/Scripts/RoadSpawn.cs
+++ BMW_Simulator/Assets/Scripts/RoadSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RoadSpawn : MonoBehaviour
 {
     [SerializeField] private Transform _player;
-    [SerializeField] private Road _roadPrefab;
+    [SerializeField] private Road[] _roadPrefab;
     [SerializeField] private Road _firstPrefab;
 
     private List<Road> Spawnedroads = new List<Road>();
@@ -17,7 +17,7 @@ public class RoadSpawn : MonoBehaviour
 
     private void Update()
     {
-        if (_player.transform.position.z > Spawnedroads[Spawnedroads.Count - 1]._end.position.z)
+        if (_player.transform.position.z > Spawnedroads[Spawnedroads.Count - 1]._end.position.z - 10)
         {
             Spawner();
         }
@@ -26,9 +26,15 @@ public class RoadSpawn : MonoBehaviour
 
     private void Spawner()
     {
-        var newroad = Instantiate(_roadPrefab);
+        var newroad = Instantiate(_roadPrefab[0]); 
         newroad.transform.position = Spawnedroads[Spawnedroads.Count - 1]._end.position - newroad._begin.localPosition;
         Spawnedroads.Add(newroad);
+
+        if (Spawnedroads.Count > 15)
+        {
+            Destroy(Spawnedroads[0].gameObject);
+            Spawnedroads.RemoveAt(0);
+        }
     }
 
 }
